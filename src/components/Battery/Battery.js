64ff40d97bf3334076e74e5f709d54bfdad0f598
zxcwer <61655281+zxcwer@users.noreply.github.com>
2022-06-20 00:48:26 +0900

    updated code

--- src/components/Battery/Battery.js
+++ src/components/Battery/Battery.js
@@ -12,8 +12,56 @@ const Battery = () => {
         batteryPercentage.innerHTML = level + "%";
 
         batteryLiquid.style.height = `${parseInt(batt.level * 100)}%`;
+
+        if (level == 100) {
+          batteryStatus.innerHTML = `Full battery <i className="ri-battery-2-fill green-color"></i>`;
+          batteryLiquid.style.height = "103%";
+        } else if (level <= 20 && batt.charging) {
+          batteryStatus.innerHTML = `Low battery <i className="ri-plug-line animated-red"></i>`;
+        } else if (batt.charging) {
+          batteryStatus.innerHTML = `Charging... <i className="ri-flashlight-line animated-green "></i>`;
+        } else {
+          batteryStatus.innerHTML = ``;
+        }
+
+        if (level <= 20) {
+          batteryLiquid.classList.add("gradient-color-red");
+          batteryLiquid.classList.remove(
+            "gradient-color-orange",
+            "gradient-color-yellow",
+            "gradient-color-green"
+          );
+        } else if (level <= 40) {
+          batteryLiquid.classList.add("gradient-color-orange");
+          batteryLiquid.classList.remove(
+            "gradient-color-red",
+            "gradient-color-yellow",
+            "gradient-color-green"
+          );
+        } else if (level <= 80) {
+          batteryLiquid.classList.add("gradient-color-yellow");
+          batteryLiquid.classList.remove(
+            "gradient-color-orange",
+            "gradient-color-red",
+            "gradient-color-green"
+          );
+        } else {
+          batteryLiquid.classList.add("gradient-color-green");
+          batteryLiquid.classList.remove(
+            "gradient-color-orange",
+            "gradient-color-yellow",
+            "gradient-color-red"
+          );
+        }
       };
       updateBattery();
+
+      batt.addEventListener("chargingchange", () => {
+        updateBattery();
+      });
+      batt.addEventListener("levelchange", () => {
+        updateBattery();
+      });
     });
   }
 
